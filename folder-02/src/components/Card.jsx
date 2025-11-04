06c64af797322b9d6d527f3ad344490145a7da73
Hamzakhanzada <khanzadahamza76@gmail.com>
2025-11-04 14:46:02 +0500

    components

---
+++ folder-02/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+      <h1>Hello I'am Hamza And I'm learning React js</h1>
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default Card
