cd45fae1fa8097f75b04dd9e8d9ac836bb1e7b80
Hamzakhanzada <khanzadahamza76@gmail.com>
2025-11-10 16:01:26 +0500

    some practice react functions

--- 07-functions/src/App.jsx
+++ 07-functions/src/App.jsx
@@ -4,10 +4,16 @@ const App = () => {
   function Click() {
       console.log("Button was clicked")
     }
+    function changingInput(val) {
+      console.log(val)
+    }
   return (
     <>
     <div>
       <button onClick={Click}>Click Here</button>
+      <input onChange={function (elem) {
+        changingInput(elem.target.value)
+      }} type="text" placeholder='Enter Your Name' />
     </div>
     </>
   )
